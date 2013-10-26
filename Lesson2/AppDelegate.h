07b6f31365ffe51d0e729bea4209fa39aa56d467
Ivan <ivanmarahovka@server.local>
2013-10-26 17:45:47 +0600

    Initial Commit

---
+++ Lesson2/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Lesson2
+//
+//  Created by Иван Мараховка on 26.10.13.
+//  Copyright (c) 2013 Иван Мараховка. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Lesson2Tests/Lesson2Tests.m
@@ -0,0 +1,34 @@
+//
+//  Lesson2Tests.m
+//  Lesson2Tests
+//
+//  Created by Иван Мараховка on 26.10.13.
+//  Copyright (c) 2013 Иван Мараховка. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface Lesson2Tests : XCTestCase
+
+@end
+
+@implementation Lesson2Tests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
